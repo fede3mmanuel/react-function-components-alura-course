4f4d208d63a9f0c30c8f57ea50b49dd9523ab8d1
Emmanuel Sanchez <fede@fede.website>
2023-05-03 11:59:17 -0600

    Layout

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import './App.css';
 import FormSignUp from './components/FormSignUp';
+import { Container } from '@mui/material';
 function App() {
   return (
-    <>
+    <Container component={"section"} maxWidth="sm">
       <h1>Formulario Registro</h1>
       <FormSignUp />
-    </>
+    </Container>
   );
 }
 
