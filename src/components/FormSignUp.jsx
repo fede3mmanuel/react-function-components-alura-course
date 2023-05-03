ab11d714061c7b508d091be50c7965052e4fb912
Emmanuel Sanchez <fede@fede.website>
2023-05-03 12:32:37 -0600

    Obteniendo valor del input

--- src/components/FormSignUp.jsx
+++ src/components/FormSignUp.jsx
@@ -1,3 +1,4 @@
+import { useState } from "react"
 import Button from "@mui/material/Button"
 import { FormGroup, TextField } from "@mui/material"
 import Switch from "@mui/material/Switch"
@@ -5,10 +6,14 @@ import FormControl from "@mui/material/FormGroup"
 import FormControlLabel from "@mui/material/FormControlLabel"
 
 function FormSignUp() {
+	const [name, setName] = useState('')
 	return (
 		<form>
 
-			<TextField id="name" label="Nombre" variant="outlined" fullWidth margin="normal"/>
+			<TextField id="name" label="Nombre" variant="outlined" fullWidth margin="normal" onChange={(e) => {
+				console.log(e.target.value);
+				setName(e.target.value)
+			}} value={ name }/>
 			<TextField id="lastName" label="Apellidos" variant="outlined" fullWidth margin="normal"/>
 			<TextField id="email" label="email" variant="outlined" fullWidth margin="normal"/>
 			
