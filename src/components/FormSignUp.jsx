17eb918fce4c336ee3ae4c5d238e54227fa630fb
Emmanuel Sanchez <fede@fede.website>
2023-05-03 13:15:11 -0600

    Finalizando gerenciamiento de formularios

--- src/components/FormSignUp.jsx
+++ src/components/FormSignUp.jsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react"
+import { useState } from "react"
 import Button from "@mui/material/Button"
 import { FormGroup, TextField } from "@mui/material"
 import Switch from "@mui/material/Switch"
@@ -7,25 +7,39 @@ import FormControlLabel from "@mui/material/FormControlLabel"
 
 function FormSignUp() {
 	const [name, setName] = useState('')
-	useEffect(() => {
-		console.log('Name cambio')
-	}, [name])
+	const [ lasName, setLastname] = useState('')
+	const [ email, setEmail] = useState('')
+	const [ prom, setProm] = useState(true)
+	const [ nov, setNov] = useState(true)
 	return (
-		<form>
+		<form onSubmit={(e) => {
+			e.preventDefault()
+			console.log({name, lasName, email, prom, nov})
+		}}>
 
 			<TextField id="name" label="Nombre" variant="outlined" fullWidth margin="normal" onChange={(e) => {
 				console.log(e.target.value);
 				setName(e.target.value)
 			}} value={ name }/>
-			<TextField id="lastName" label="Apellidos" variant="outlined" fullWidth margin="normal"/>
-			<TextField id="email" label="email" variant="outlined" fullWidth margin="normal"/>
+			<TextField id="lastName" label="Apellidos" variant="outlined" fullWidth margin="normal" onChange={(e) => {
+				setLastname(e.target.value)
+			}} value={ lasName }/>
+			<TextField id="email" label="email" variant="outlined" fullWidth margin="normal" onChange={(e) => {
+				setEmail(e.target.value)
+			}} value={ email }/>
 			
 			<FormGroup>
-				<FormControlLabel control={ <Switch defaultChecked /> } label="Prommociones"/>
-				<FormControlLabel control={ <Switch defaultChecked /> } label="Novedades"/>
+				<FormControlLabel control={ <Switch checked={prom} onChange={(e) => {
+					setProm(e.target.checked)
+					console.log(e.target.checked);
+				}} /> } label="Prommociones"/>
+				<FormControlLabel control={ <Switch checked={nov} onChange={(e) => {
+					setNov(e.target.checked)
+					console.log(e.target.checked);
+				}}/> } label="Novedades"/>
 			</FormGroup>
 			
-			<Button variant="contained">
+			<Button variant="contained" type="submit">
 				Registrarse
 			</Button>
 		</form>
